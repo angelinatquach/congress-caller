bbe90bc2124671b710023cfc93182eb25b3ac734
angelinatquach <angelinatquach.gmail.com>
2020-06-06 00:41:01 -0700

    Update get env call

--- congress-caller.py
+++ congress-caller.py
@@ -1,47 +1,70 @@
 from twilio.rest import Client
 from datetime import datetime
-from boto.s3.connection import S3Connection
+from itertools import chain
+import names
+import requests
 import time
 import os
 
 def callCongress():
-	account_sid = S3Connection(os.environ['ACCOUNT_SID'])
-	auth_token = S3Connection(os.environ['AUTH_TOKEN'])
-	fromNumber = S3Connection(os.environ['TWILIO_PHONE'])
-	#phoneNumbers = ['16503420300', '2022253531'];
-	phoneNumbers = ['7147208654'];
-	message = '''<Response><Say voice="Polly.Emma" language="en-US"> Hello, Congress woman Jackie Speier. My name is Angelina, and I am a
-	California resident. It is very important to me that you pass the End Qualified Immunity Act. Qualified immunity means that victims of brutality or harassment 
-	by law enforcement almost always get no relief in court and have no ability to hold offending officers accountable for their actions.
-	That means the officers who commit the brutality and harassment that we are witnessing in the news today, and the governments that employ them,
-	have little incentive to improve their practices, follow the law, and protect the people. Police are legally, politically, and culturally insulated
-	from consequences for violating the rights of the people whom they have sworn to serve. That must change immediately so that these incidents of
-	brutality stop happening.
+    #TODO: Get ProPublica and Twilio Api Key/Auth Tokens, save as env variables
+    api_key = os.environ['API_KEY']
+    account_sid = os.environ['ACCOUNT_SID']
+    auth_token = os.environ['AUTH_TOKEN']
+    fromNumber = os.environ['TWILIO_PHONE']
 
-	Ending qualified immunity would restore Americans' ability to obtain relief when police officers violate their constitutionally secured rights. It would
-	also provide a powerful incentive for municipalities to restructure their law enforcement agencies and adopt policies and practices that curtail 
-	abuses of power.
+    senateUrl = "https://api.propublica.org/congress/v1/116/senate/members.json"
+    houseUrl = "https://api.propublica.org/congress/v1/116/house/members.json"
+    headers = {'x-api-key': api_key}
 
-	I urge you to vote YES to pass the End Qualified Immunity Act. Thanks for your time and attention today! </Say></Response>''';
+    #TODO: Replace {2} with your name if you'd like. Otherwise, it's randomized.
+    message = '''<Response><Say voice="Polly.Emma" language="en-US"> Hello, Congress leader {0} {1}. My name is {2}, and I am a
+    California resident. It is very important to me that you pass the End Qualified Immunity Act. Qualified immunity means that victims of brutality or harassment 
+    by law enforcement almost always get no relief in court and have no ability to hold offending officers accountable for their actions.
+    That means the officers who commit the brutality and harassment that we are witnessing in the news today, and the governments that employ them,
+    have little incentive to improve their practices, follow the law, and protect the people. Police are legally, politically, and culturally insulated
+    from consequences for violating the rights of the people whom they have sworn to serve. That must change immediately so that these incidents of
+    brutality stop happening.
 
-	client = Client(account_sid, auth_token)
+    Ending qualified immunity would restore Americans' ability to obtain relief when police officers violate their constitutionally secured rights. It would
+    also provide a powerful incentive for municipalities to restructure their law enforcement agencies and adopt policies and practices that curtail 
+    abuses of power.
 
-	while True:
-		for toNumber in phoneNumbers:
-			print("Time of call: %s" % (datetime.now()))
+    I urge you to vote YES to pass the End Qualified Immunity Act. Thanks for your time and attention today! </Say></Response>''';
 
-			call = client.calls.create(
-			                        status_callback_event = ['initiated', 'answered'],
-			                        twiml = message,
-			                        to = toNumber,
-			                        from_ = fromNumber
-			                    )
-			print("Call SID: %s" % (call.sid))
-			time.sleep(900)
+    callerClient = Client(account_sid, auth_token)
+    senateMembers = requests.get(senateUrl, headers=headers).json()["results"][0]["members"]
+    houseMembers = requests.get(houseUrl, headers=headers).json()["results"][0]["members"]
+
+    #TODO: Replace CA with your state
+    senate = (member for member in senateMembers if member["state"] == "CA")
+    house = (member for member in houseMembers if member["state"] == "CA")
+    congress = chain(senate, house)
+
+    while True:
+        for member in congress:
+            memberFirstName = member["first_name"]
+            memberLastName = member["last_name"]   
+            memberPhoneNumber = member["phone"]
+            callerName = names.get_first_name(gender='female')
+
+            print("[%s] Time to call %s %s" % (datetime.now(), memberFirstName, memberLastName))
+
+            call = callerClient.calls.create(
+                                   status_callback_event = ['initiated', 'answered'],
+                                   twiml = message.format(memberFirstName, memberLastName, callerName),
+                                   to = memberPhoneNumber,
+                                   from_ = fromNumber
+                               )
+
+            print("Call SID: %s" % (call.sid))
+
+            #calls every 30 minutes
+            time.sleep(1800)            
 
 def main():
-	print('Initializing congress caller bot\n')
-	callCongress()
+    print('Initializing Congress caller bot\n')
+    callCongress()
 
 
 if __name__ == '__main__':
